91d9a0bfc144fff3cb78e3cf1ffa3ed195496882
gab-berest <gabriel.berestovoy@gmail.com>
2020-12-26 13:07:42 -0500

    Change prescaler

--- main/main.ino
+++ main/main.ino
@@ -88,7 +88,7 @@ void setupMotorTimer() {
   TCNT1 = 0;
   OCR1A = 1;
   TCCR1B |= (1 << WGM12);
-  TCCR1B |= (1 << CS12) | (1 << CS10);
+  TCCR1B |= (1 << CS11) | (1 << CS10);
   TIMSK1 |= (1 << OCIE1A);
   interrupts();
 }
